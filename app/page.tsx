12da47b214f7359dfcff0a94578cc02ac7e051ed
Yash Maurya <yashmaurya7812@gmail.com>
2024-09-07 18:47:30 +0530

    Added icon for pause/play

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-"use client"
-import { useEffect, useState } from 'react';
-import Image from 'next/image'; // Importing Image from next/image for image optimization
-import { screens } from './data';
-
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image"; // Importing Image from next/image for image optimization
+import { screens } from "./data";
+import { IconPlayerPause, IconPlayerPlay } from "@tabler/icons-react";
 interface SliderProps {
   idx: number;
 }
@@ -13,13 +13,21 @@ const commonSliderThumb = (idx: number, baseClass: string) =>
 
 // Mobile slider component
 const MobileSlider: React.FC<SliderProps> = ({ idx }) => (
-  <div className='min-h-[60px] w-full md:hidden flex-center p-2'>
-    <div className='w-full max-w-[400px] bg-slider-bg h-[3px] rounded-full'>
-      <div 
-        className={`${commonSliderThumb(idx, "relative h-full transition-all ease-out rounded-full")}`} 
-        style={{ width: screens[idx]?.slider || '0%' }} // Safe access for slider width
+  <div className="min-h-[60px] w-full md:hidden flex-center p-2">
+    <div className="w-full max-w-[400px] bg-slider-bg h-[3px] rounded-full">
+      <div
+        className={`${commonSliderThumb(
+          idx,
+          "relative h-full transition-all ease-out rounded-full"
+        )}`}
+        style={{ width: screens[idx]?.slider || "0%" }} // Safe access for slider width
       >
-        <div className={`${commonSliderThumb(idx, "sliderthumb -translate-y-1/2 top-1/2 right-0")}`}></div>
+        <div
+          className={`${commonSliderThumb(
+            idx,
+            "sliderthumb -translate-y-1/2 top-1/2 right-0"
+          )}`}
+        ></div>
       </div>
     </div>
   </div>
@@ -27,13 +35,21 @@ const MobileSlider: React.FC<SliderProps> = ({ idx }) => (
 
 // Desktop slider component
 const DesktopSlider: React.FC<SliderProps> = ({ idx }) => (
-  <div className='h-full w-full max-w-[300px] max-md:hidden flex-center p-2'>
-    <div className='h-full max-h-[400px] bg-slider-bg w-[3px] rounded-full'>
-      <div 
-        className={`${commonSliderThumb(idx, "relative h-full transition-all ease-out rounded-full")}`} 
-        style={{ height: screens[idx]?.slider || '0%' }} // Safe access for slider height
+  <div className="h-full w-full max-w-[300px] max-md:hidden flex-center p-2">
+    <div className="h-full max-h-[400px] bg-slider-bg w-[3px] rounded-full">
+      <div
+        className={`${commonSliderThumb(
+          idx,
+          "relative h-full transition-all ease-out rounded-full"
+        )}`}
+        style={{ height: screens[idx]?.slider || "0%" }} // Safe access for slider height
       >
-        <div className={`${commonSliderThumb(idx, "sliderthumb -translate-x-1/2 left-1/2 bottom-0")}`}></div>
+        <div
+          className={`${commonSliderThumb(
+            idx,
+            "sliderthumb -translate-x-1/2 left-1/2 bottom-0"
+          )}`}
+        ></div>
       </div>
     </div>
   </div>
@@ -41,14 +57,16 @@ const DesktopSlider: React.FC<SliderProps> = ({ idx }) => (
 
 // Content component displaying title and description
 const Content: React.FC<SliderProps> = ({ idx }) => (
-  <div className='flex-col-center w-full max-w-[380px] md:max-w-[422px]'>
-    <div className='flex-col-start gap-y-[16px] md:gap-y-[48px] p-4 py-2 w-full'>
-      <h1 className='text-header md:text-header-lg md:leading-[64px] text-header-gray font-medium max-w-[283px]'>
-        {screens[idx]?.title || 'Title'}  {/* Safe access with fallback for title */}
+  <div className="flex-col-center w-full max-w-[380px] md:max-w-[422px]">
+    <div className="flex-col-start gap-y-[16px] md:gap-y-[48px] p-4 py-2 w-full">
+      <h1 className="text-header md:text-header-lg md:leading-[64px] text-header-gray font-medium max-w-[283px]">
+        {screens[idx]?.title || "Title"}{" "}
+        {/* Safe access with fallback for title */}
       </h1>
-      <span className='h-[2px] bg-line-blue w-[85px] rounded-full'></span>
-      <div className='w-full text-description md:text-description-lg md:leading-[28px]'>
-        {screens[idx]?.description || 'Description'}  {/* Safe access with fallback for description */}
+      <span className="h-[2px] bg-line-blue w-[85px] rounded-full"></span>
+      <div className="w-full text-description md:text-description-lg md:leading-[28px]">
+        {screens[idx]?.description || "Description"}{" "}
+        {/* Safe access with fallback for description */}
       </div>
     </div>
   </div>
@@ -56,22 +74,22 @@ const Content: React.FC<SliderProps> = ({ idx }) => (
 
 // Image and arrow component
 const ImageAndArrow: React.FC<SliderProps> = ({ idx }) => (
-  <div className='h-[calc(100%-206px)] w-fit md:h-full relative md:max-h-screen md:w-fit min-w-[340px] max-w-[350px] flex-shrink'>
-    <Image  
-      src={screens[idx]?.image || ''}  // Safe access with fallback for image src
-      alt="onboarding image" 
-      layout="fill"
-      objectFit="contain"
-      className={`h-full max-md:mx-auto md:w-full md:h-auto flex-shrink md:flex-shrink-0 md:absolute transition-all duration-300 ease-out ${screens[idx]?.img_layout || ''}`} 
+  <div className="h-[calc(100%-200px)] w-fit md:h-full relative md:max-h-[800px] md:w-fit min-w-[340px] max-w-[350px] flex-shrink">
+    <Image
+      src={screens[idx].image}
+      alt="onboarding image"
+      width={500}
+      height={500}
+      className={`h-full max-md:mx-auto md:w-full md:h-auto flex-shrink md:flex-shrink-0 md:absolute transition-all duration-300 ease-out ${screens[idx].img_layout}`}
     />
     {screens[idx]?.arrow && (
-      <Image 
-        src='/arrow.svg' 
-        alt="indication arrow" 
-        layout="fill"
-        objectFit="contain"
-        className={`max-md:hidden w-full h-auto absolute transition-all duration-300 ${screens[idx].arrow}`} 
-        style={screens[idx]?.arrow_additional || {}}  // Optional inline styling for arrow
+      <Image
+        width={500}
+        height={500}
+        src="/arrow.svg"
+        alt="indication arrow"
+        className={`max-md:hidden w-full h-auto absolute transition-all duration-300 ${screens[idx].arrow}`}
+        style={screens[idx]?.arrow_additional || {}}
       />
     )}
   </div>
@@ -85,13 +103,13 @@ export default function Home() {
   // Keydown listener to toggle auto-scroll on spacebar press
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === ' ' && !event.repeat) {
-        setToggle(prevToggle => !prevToggle); // Toggle auto-scroll
+      if (event.key === " " && !event.repeat) {
+        setToggle((prevToggle) => !prevToggle); // Toggle auto-scroll
       }
     };
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown); // Clean up listener
+      document.removeEventListener("keydown", handleKeyDown); // Clean up listener
     };
   }, []);
 
@@ -113,16 +131,29 @@ export default function Home() {
       <div className="container relative z-10 flex-col-center md:flex-row-reverse items-center justify-center h-full">
         <MobileSlider idx={idx} />
         <DesktopSlider idx={idx} />
-        <div className='flex-col-center md:flex-row w-full md:w-[calc(100%-300px)] z-10 h-[calc(100%-60px)] md:h-full gap-y-5 md:gap-x-[80px] items-center justify-center'>
+        <div className="flex-col-center md:flex-row w-full md:w-[calc(100%-300px)] z-10 h-[calc(100%-60px)] md:h-full gap-y-5 md:gap-x-[80px] items-center justify-center">
           <Content idx={idx} />
           <ImageAndArrow idx={idx} />
         </div>
       </div>
       {/* Pause/Play indicator */}
-      <div className='text-neutral-400 hidden md:flex bg-black/20 rounded-full px-4 py-2 absolute bottom-2 left-2 items-center shadow-[#3B95FF]/70 shadow'>
-        {toggle ? <span className='bg-[#3B95FF] text-white rounded-lg text-xs p-1'>Paused</span> : <span>Click <span className='bg-[#3B95FF] text-neutral-300 mx-1 text-xs p-1 rounded-sm border-b'>space</span> to Pause</span>}
+      <div className="text-neutral-400 hidden md:flex bg-black/20 rounded-full px-4 py-2 absolute bottom-2 left-2 items-center shadow-[#3B95FF]/70 shadow">
+        {toggle ? (
+          <IconPlayerPlay className="mx-2" />
+        ) : (
+          <IconPlayerPause className="mx-2" />
+        )}
+        {toggle ? (
+          <span>
+            Press <kbd className="mx-2">Space</kbd> to resume
+          </span>
+        ) : (
+          <span>
+            Press <kbd className="mx-2">Space</kbd> to pause
+          </span>
+        )}
       </div>
-      <div className='bg-black/20 w-full h-[10%] absolute right-0 bottom-0 md:top-0 md:w-[40%] md:h-full'></div>
+      <div className="bg-black/20 w-full h-[10%] absolute right-0 bottom-0 md:top-0 md:w-[40%] md:h-full"></div>
     </section>
   );
 }
